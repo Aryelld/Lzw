851da4ffb0928f4fd7eb41579a3f63332e11ef9c
Aryell Dias <aryelld@gmail.com>
2017-07-12 20:51:43 -0300

    inacabado

--- Lzw.hs
+++ Lzw.hs
@@ -12,8 +12,15 @@ aux 1 y (h:t) = (h:(aux 1 (y-1) t))
 aux x y (h:t) = aux (x-1) y t
 
 descompacta (C comeco (C meio restoDaExpressao)) = descompacta (C (comeco++meio) restoDaExpressao)
-descompacta (C comeco (T x y restoDaExpressao)) = descompacta (C (comeco++adiante) restoDaExpressao)
-    where
-        adiante = aux x y comeco
+descompacta (C comeco (T x y restoDaExpressao)) = descompacta (C (comeco++(aux x y comeco)) restoDaExpressao)
 descompacta (C comeco (C fim End)) = comeco++fim
 descompacta (C comeco End) = comeco
+
+compacta::Eq a => [a] -> Lzw a
+compacta [] = []
+compacta l = compactador l [] End
+
+compactador (h:[]) [] a = C (filter (\x-> (x /= '\'') (show h)) a
+compactador (h:t) [] a = 
+
+aux (h1:t1) (h2:t2) = 
