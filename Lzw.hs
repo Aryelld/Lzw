008f700ff2c81fbab8a4a9cffcaed57d1365c9e8
Aryell Dias <aryelld@gmail.com>
2017-07-13 11:22:54 -0300

    excluindo coisas inuteis

--- Lzw.hs
+++ Lzw.hs
@@ -18,46 +18,35 @@ descompacta (C comeco (T x y restoDaExpressao)) = descompacta (C (comeco++(aux x
  
 compacta [] = C [] End
 compacta l = compactador l [] 
-
 compactador [] _ = simpl (C [] End)
-compactador sera jafoi = retorno
+compactador sera jafoi = 
+    if (posicao>=1)&&(extensao>1) then  
+        (T posicao extensao (simpl (compactador diante historico )))
+    else 
+        simpl (C [(last historico)] (compactador diante historico))
     where 
-        (posicao, extensao,historico, diante) = compAux sera jafoi
-        retorno = 
-            if (posicao>=1)&&(extensao>1) then  
-                (T posicao extensao (simpl (compactador diante historico )))
-            else 
-            -- if n <= (length historico) then --se não passar do 
-                simpl (C [(last historico)] (compactador diante historico))
-            -- else 
-            --     End
-
+        (posicao, extensao,historico, diante) = mineradorDeDados sera jafoi
+                
 simpl (C a (C b c)) = simpl (C (a++b) c) 
 simpl a = a
-
-compAux [] _ = (0,0,[],[])
-compAux (h:t) [] = (0,0,(h:[]),t)
-compAux a b = 
+ 
+mineradorDeDados  [] _ = (0,0,[],[])
+mineradorDeDados (h:t) [] = (0,0,(h:[]),t)
+mineradorDeDados a b = 
     (posicao, extensao,historico,diante)
     where
-        historico = b++take aux a
-        aux = 
-            if extensao == 0 then
-                1
-            else 
-                extensao 
-        diante = drop aux a
+        historico = b++take extensaoUtilisavel a
+        extensaoUtilisavel = if extensao == 0 then 1 else extensao 
+        diante = drop extensaoUtilisavel a
         posicao = aux1 a b
         extensao = aux2 a (drop (posicao-1) b) False
 
-aux1 [] _ = 0
 aux1 _ [] = 0
 aux1 (h1:t1) (h2:t2)
     | (h1 == h2) = 1 
     | otherwise = 1+(aux1 (h1:t1) (t2) )
 
 aux2 [] _ _= 0
-aux2 _ [] _= 0
 aux2 (h1:t1) (h2:t2) bool= 
     if (h1==h2)&&(bool||a/=0) then 1 + a else 0
         where
