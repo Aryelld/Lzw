44d548a1129f42b360d09e240a9c54322b3552f0
Aryell Dias <aryelld@gmail.com>
2017-07-13 03:22:11 -0300

    necessario organizar e tratar erros

--- Lzw.hs
+++ Lzw.hs
@@ -15,37 +15,44 @@ descompacta (C comeco End) = comeco
 descompacta (C comeco (C fim End)) = comeco++fim
 descompacta (C comeco (C meio restoDaExpressao)) = descompacta (C (comeco++meio) restoDaExpressao)
 descompacta (C comeco (T x y restoDaExpressao)) = descompacta (C (comeco++(aux x y comeco)) restoDaExpressao)
+ 
+compacta [] = C [] End
+compacta l = compactador l [] 0
 
-compacta [] = []
-compacta l = compactador l [] End
+compactador [] _ _ = simpl (C [] End)
+compactador sera jafoi n = retorno
+    where 
+        (posicao, extensao,historico, diante) = compAux sera jafoi
+        retorno = 
+            if (posicao>=1)&&(extensao>1) then  
+                (T posicao extensao (simpl (compactador diante historico (n+extensao))))
+            else if n <= (length historico) then --se não passar do 
+                simpl (C [(historico!!n)] (compactador diante historico (n+1)))
+            else 
+                End
 
--- compactador (h:[]) [] a = C (h:[]) a
--- compactador (h:t) [] a = C (h:[]) (compactador t (h:[]) a)
--- compactador (h:t) (l) a = retorno
---     where 
---         (posicao, extensao, historico, diante) = aux3 (h:t) l
---         retorno = if posicao==0 then (C historico End) else 
+simpl (C a (C b c)) = simpl (C (a++b) c) 
+simpl a = a
 
-aux1 [] _ n = 0
-aux1 _ [] n = 0
+compAux [] _ = (0,0,[],[])
+compAux (h:t) [] = (0,0,(h:[]),t)
+compAux a b = 
+    (posicao, extensao,historico,drop (posicao+extensao+1) a)
+    where
+        historico = b++aj 
+        aj = (take (posicao+extensao+1) a)
+        posicao = aux1 a b 0
+        extensao = aux2 a (drop (posicao-1) b) False
+
+aux1 [] _ n = 0-n
+aux1 _ [] n = 0-n
 aux1 (h1:t1) (h2:t2) n
-    | (h1 == h2) = n
-    | otherwise = (aux1 t1 (h2:t2) (n + 1))
-
+    | (h1 == h2) = 1 
+    | otherwise = 1+(aux1 (h1:t1) (t2) (n + 1))
 
 aux2 [] _ _= 0
 aux2 _ [] _= 0
 aux2 (h1:t1) (h2:t2) bool= 
     if (h1==h2)&&(bool||a/=0) then 1 + a else 0
-    where
-        a = (aux2 t1 t2 (h1==h2))
-
---funcao recebe dois argumentos que representao a lista a ser analisada, e o historico
---sob essas condicoes ela analisa essas duas listas sob o ponto de vista de aux2 e aux1
---obtendo a posicao, estensao, string anterior e a que ainda será analisada numa tupla
-aux3 [] _ = (0,0,[],[])
-aux3 a [] = (0,0,a,[])
-aux3 a b = (posicao+1, extensao,(take posicao a),drop (posicao+extensao) a)
-    where
-        posicao = aux1 a b 0
-        extensao = aux2 (drop (posicao) a) b False
+        where
+            a = (aux2 t1 t2 (h1==h2))
