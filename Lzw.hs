39590f65066ce12c1709f85ddf5c871c0c6e8f32
Aldo Monteiro <aldo.fsmonteiro@gmail.com>
2017-07-13 20:24:44 -0300

    simplificações

--- Lzw.hs
+++ Lzw.hs
@@ -5,14 +5,14 @@ data Lzw a = C [a] (Lzw a) | T Int Int (Lzw a) | End
 
 instance Show a => Show (Lzw a) where
     show (C [] b) = show b
-    show (C a b) = filter (\x->x/='"') ((show a) ++ (show b))
+    show (C a b) = filter (/='"') ((show a) ++ (show b))
     show (T a b c) = "(T" ++ (show a) ++ "," ++ (show b) ++ ")" ++ (show c)
     show (End) = ""
 
 subs x y l
     |l == [] = []
     |y == 0 = []    
-    |x == 1 = ((head l):(subs 1 (y-1) (tail l)))
+    |x == 1 = (head l):(subs 1 (y-1) (tail l))
     |otherwise = subs (x-1) y (tail l)
 
 descompacta (C comeco End) = comeco
@@ -25,8 +25,8 @@ compacta l = compactador l []
 
 compactador [] _ = C [] End
 compactador sera jafoi = 
-    if (posicao>=1)&&(extensao>1) then  
-        (T posicao extensao (simpl (compactador diante historico )))
+    if posicao >= 1 && extensao > 1 then  
+        T posicao extensao (simpl (compactador diante historico ))
     else 
         simpl (C [(last historico)] (compactador diante historico))
     where 
@@ -36,8 +36,8 @@ simpl (C a (C b c)) = simpl (C (a++b) c)
 simpl a = a
 
 mineradorDeDados  [] _ = (0,0,[],[])
-mineradorDeDados (h:t) [] = (0,0,(h:[]),t)
-mineradorDeDados sera jafoi = (posicao, extensao,historico,diante)
+mineradorDeDados (h:t) [] = (0, 0, h:[], t)
+mineradorDeDados sera jafoi = (posicao, extensao, historico, diante)
     where
         historico = jafoi ++ (take extensaoUtilisavel sera)
         diante = drop extensaoUtilisavel sera
@@ -49,11 +49,11 @@ pos _ [] = 0
 pos [] _ = 0
 pos (h1:t1) (h2:t2)
     | (h1 == h2)&&((ext (h1:t1) (h2:t2) False)/=0)= 1 
-    | otherwise = 1+(pos (h1:t1) (t2) )
+    | otherwise = 1 + (pos (h1:t1) (t2))
 
 ext [] _ _= 0
 ext _ [] _ = 0
 ext (h1:t1) (h2:t2) bool= 
     if (h1==h2)&&(bool||a/=0) then 1 + a else 0
         where
-            a = (ext t1 t2 (h1==h2))
+            a = ext t1 t2 (h1==h2)
