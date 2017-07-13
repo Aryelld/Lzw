4bb7580871c9e867ef89f5fbcc655e26113b1d24
Aryell Dias <aryelld@gmail.com>
2017-07-12 22:13:46 -0300

    criado metodos auxiliares que seram uteis

--- Lzw.hs
+++ Lzw.hs
@@ -11,16 +11,41 @@ aux _ 0 _ = []
 aux 1 y (h:t) = (h:(aux 1 (y-1) t))
 aux x y (h:t) = aux (x-1) y t
 
+descompacta (C comeco End) = comeco
+descompacta (C comeco (C fim End)) = comeco++fim
 descompacta (C comeco (C meio restoDaExpressao)) = descompacta (C (comeco++meio) restoDaExpressao)
 descompacta (C comeco (T x y restoDaExpressao)) = descompacta (C (comeco++(aux x y comeco)) restoDaExpressao)
-descompacta (C comeco (C fim End)) = comeco++fim
-descompacta (C comeco End) = comeco
 
-compacta::Eq a => [a] -> Lzw a
 compacta [] = []
 compacta l = compactador l [] End
 
-compactador (h:[]) [] a = C (filter (\x-> (x /= '\'') (show h)) a
-compactador (h:t) [] a = 
+-- compactador (h:[]) [] a = C (h:[]) a
+-- compactador (h:t) [] a = C (h:[]) (compactador t (h:[]) a)
+-- compactador (h:t) (l) a = retorno
+--     where 
+--         (posicao, extensao, historico, diante) = aux3 (h:t) l
+--         retorno = if posicao==0 then (C historico End) else 
 
-aux (h1:t1) (h2:t2) = 
+aux1 [] _ n = 0
+aux1 _ [] n = 0
+aux1 (h1:t1) (h2:t2) n
+    | (h1 == h2) = n
+    | otherwise = (aux1 t1 (h2:t2) (n + 1))
+
+
+aux2 [] _ _= 0
+aux2 _ [] _= 0
+aux2 (h1:t1) (h2:t2) bool= 
+    if (h1==h2)&&(bool||a/=0) then 1 + a else 0
+    where
+        a = (aux2 t1 t2 (h1==h2))
+
+--funcao recebe dois argumentos que representao a lista a ser analisada, e o historico
+--sob essas condicoes ela analisa essas duas listas sob o ponto de vista de aux2 e aux1
+--obtendo a posicao, estensao, string anterior e a que ainda será analisada numa tupla
+aux3 [] _ = (0,0,[],[])
+aux3 a [] = (0,0,a,[])
+aux3 a b = (posicao+1, extensao,(take posicao a),drop (posicao+extensao) a)
+    where
+        posicao = aux1 a b 0
+        extensao = aux2 (drop (posicao) a) b False
