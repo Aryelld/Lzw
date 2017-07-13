91e3e479b07f4383192efec2c9d6b86f3897116a
Aryell Dias <aryelld@gmail.com>
2017-07-13 11:10:43 -0300

    fiz pegar perfeitamente, apenas falta organizar o codigo

--- Lzw.hs
+++ Lzw.hs
@@ -17,19 +17,20 @@ descompacta (C comeco (C meio restoDaExpressao)) = descompacta (C (comeco++meio)
 descompacta (C comeco (T x y restoDaExpressao)) = descompacta (C (comeco++(aux x y comeco)) restoDaExpressao)
  
 compacta [] = C [] End
-compacta l = compactador l [] 0
+compacta l = compactador l [] 
 
-compactador [] _ _ = simpl (C [] End)
-compactador sera jafoi n = retorno
+compactador [] _ = simpl (C [] End)
+compactador sera jafoi = retorno
     where 
         (posicao, extensao,historico, diante) = compAux sera jafoi
         retorno = 
             if (posicao>=1)&&(extensao>1) then  
-                (T posicao extensao (simpl (compactador diante historico (n+extensao))))
-            else if n <= (length historico) then --se não passar do 
-                simpl (C [(historico!!n)] (compactador diante historico (n+1)))
+                (T posicao extensao (simpl (compactador diante historico )))
             else 
-                End
+            -- if n <= (length historico) then --se não passar do 
+                simpl (C [(last historico)] (compactador diante historico))
+            -- else 
+            --     End
 
 simpl (C a (C b c)) = simpl (C (a++b) c) 
 simpl a = a
@@ -37,18 +38,23 @@ simpl a = a
 compAux [] _ = (0,0,[],[])
 compAux (h:t) [] = (0,0,(h:[]),t)
 compAux a b = 
-    (posicao, extensao,historico,drop (posicao+extensao+1) a)
+    (posicao, extensao,historico,diante)
     where
-        historico = b++aj 
-        aj = (take (posicao+extensao+1) a)
-        posicao = aux1 a b 0
+        historico = b++take aux a
+        aux = 
+            if extensao == 0 then
+                1
+            else 
+                extensao 
+        diante = drop aux a
+        posicao = aux1 a b
         extensao = aux2 a (drop (posicao-1) b) False
 
-aux1 [] _ n = 0-n
-aux1 _ [] n = 0-n
-aux1 (h1:t1) (h2:t2) n
+aux1 [] _ = 0
+aux1 _ [] = 0
+aux1 (h1:t1) (h2:t2)
     | (h1 == h2) = 1 
-    | otherwise = 1+(aux1 (h1:t1) (t2) (n + 1))
+    | otherwise = 1+(aux1 (h1:t1) (t2) )
 
 aux2 [] _ _= 0
 aux2 _ [] _= 0
